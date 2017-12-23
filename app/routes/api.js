428fa8d09f3fe7883bc105e5644d50fac0572d45
David Basil <info@basildavid.com>
2017-12-23 17:32:08 +0400

    Move routes to their own file

---
+++ app/routes/api.js
@@ -0,0 +1,23 @@
+var User = require('../models/user')
+
+module.exports = function(router) {
+	router.post('/users', function(req, res){
+		var user = new User()
+		user.username = req.body.username
+		user.password = req.body.password
+		user.email = req.body.email
+		var query = req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == ''
+		if (query){
+			res.send('Ensure username, email and password are provided')
+		} else {
+			user.save(function(err){
+				if(err){
+					res.send('Username or Email already exists')
+				} else {
+					res.send('user created')
+				}
+			})
+		}
+	})
+	return router
+}
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var app = express()
 var port = process.env.PORT || 8000
 var morgan = require('morgan')
 var mongoose = require('mongoose')
-var User = require('./app/models/user')
 var bodyParser = require('body-parser')
-
-// body parser
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+var router = express.Router()
+var appRoutes = require('./app/routes/api')(router)
 
 // morgan
 app.use(morgan('dev'))
+// body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/api', appRoutes)
+
 
 // mongoose connection
 mongoose.connect('mongodb://localhost/tutorial', function(err){
@@ -19,26 +21,6 @@ mongoose.connect('mongodb://localhost/tutorial', function(err){
 		console.log('Not connected to the database: ' + err)
 	} else {
 		console.log('Successfully connected to the database')
-	}
-})
-
-// routes
-app.post('/users', function(req, res){
-	var user = new User()
-	user.username = req.body.username
-	user.password = req.body.password
-	user.email = req.body.email
-	var query = req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.email == null || req.body.email == ''
-	if (query){
-		res.send('Ensure username, email and password are provided')
-	} else {
-		user.save(function(err){
-			if(err){
-				res.send('Username or Email already exists')
-			} else {
-				res.send('user created')
-			}
-		})
 	}
 })
 
