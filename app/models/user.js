64cb03344f09a154f57b15fa9244cbbe2ef16505
David Basil <info@basildavid.com>
2017-12-23 13:25:57 +0400

    add bcrypt-nodejs

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
+var bcrypt = require('bcrypt-nodejs')
 
 // user schema
 var UserSchema = new Schema({
@@ -20,5 +21,16 @@ var UserSchema = new Schema({
 	}
 })
 
+// bcrypt(hash) user password
+UserSchema.pre('save', function(){
+	var user = this
+	bcrypt.hash(user.password, null, null, function(err, hash){
+		if(err){
+			return next(err)
+		}
+		user.password = hash
+		next()
+	})
+})
 
 module.exports = mongoose.model('User', UserSchema)
